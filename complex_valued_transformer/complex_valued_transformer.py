611f88ce4b52aa3272a275f5af1199f95376ec9c
lucidrains <lucidrains@gmail.com>
2023-10-08 16:02:00 -0700

    add the complex attention proposed by Yang et al. as well to play around with

--- complex_valued_transformer/complex_valued_transformer.py
+++ complex_valued_transformer/complex_valued_transformer.py
@@ -6,24 +6,23 @@ import torch.nn.functional as F
 from torch import nn, einsum, Tensor
 from torch.nn import Module
 
-from einops import rearrange, repeat
+from einops import rearrange, repeat, reduce
 
 # helpers
 
 def exists(v):
     return v is not None
 
-# complex attention
+# complex attention - Eilers et al
+# https://arxiv.org/abs/2306.09827
 
-def complex_attention(
+def eilers_complex_attention(
     q: Tensor,
     k: Tensor,
     v: Tensor,
     mask: Optional[Tensor] = None
 ):
     """
-    https://arxiv.org/abs/2306.09827
-
     section 4.1 equation 8
     """
 
@@ -50,3 +49,48 @@ def complex_attention(
     attn = attn + 0j
     out = einsum('b h i j, b h j d -> b h i d', attn, v)
     return out
+
+# complex attention - Yang et al
+# https://arxiv.org/abs/1910.10202
+
+def yang_complex_attention(
+    q: Tensor,
+    k: Tensor,
+    v: Tensor,
+    mask: Optional[Tensor] = None
+):
+    """
+    section 3.2 equation 3
+    """
+    device = q.device
+
+    assert all([t.dtype == cfloat for t in (q, k, v)])
+    q, k, v = [torch.view_as_real(t) for t in (q, k, v)]
+
+    q = repeat(q, 'b h n d c -> (r1 c r2) b h n d', r1 = 2, r2 = 2)
+    k = repeat(k, 'b h n d c -> (r c) b h n d', r = 4)
+    v = repeat(v, 'b h n d c -> (r c) b h n d', r = 4)
+
+    scale = q.shape[-1] ** -0.5
+
+    sim = einsum('... i d, ... j d -> ... i j', q, k)
+    sim = sim * scale
+
+    dtype = sim.dtype
+
+    if exists(mask):
+        mask = rearrange(mask, 'b j -> 1 b 1 1 j')
+        sim = sim.masked_fill(~mask, -torch.finfo(dtype).max)
+
+    attn = sim.softmax(dim = -1)
+
+    o = einsum('c ... i j, c ... j d -> ... i d c', attn, v)
+
+    sign = torch.tensor([
+        1., -1., -1., -1.,   # real component
+        1.,  1.,  1., -1.    # imag component
+    ], dtype = dtype, device = device)
+
+    o = reduce(o * sign, 'b h n d (c r) -> b h n d c', 'sum', c = 2)
+
+    return torch.view_as_complex(o)
